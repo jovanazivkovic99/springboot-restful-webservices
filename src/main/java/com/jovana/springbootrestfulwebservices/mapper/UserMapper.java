fb48f67e49bb00e4c7596c5f3de4dbcbf9c13ad0
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-12 17:17:02 +0200

    Added user mapping mapstruct interface

---
+++ src/main/java/com/jovana/springbootrestfulwebservices/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.jovana.springbootrestfulwebservices.mapper;
+
+import com.jovana.springbootrestfulwebservices.dto.UserDto;
+import com.jovana.springbootrestfulwebservices.entity.User;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public interface UserMapper {
+
+    UserDto userToUserDto(User user);
+    User userDtoToUser(UserDto userDto);
+}
