2e95b9c0411ab70d392556655e5cf714807fe270
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-13 13:42:23 +0200

    Added validations

--- src/main/java/com/jovana/springbootrestfulwebservices/dto/UserDto.java
+++ src/main/java/com/jovana/springbootrestfulwebservices/dto/UserDto.java
@@ -1,8 +1,14 @@
 package com.jovana.springbootrestfulwebservices.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
-public record UserDto(Long id, String firstName, String lastName, String email) {
+public record UserDto(Long id,
+                      @NotEmpty // should not be null or empty
+                      String firstName,
+                      @NotEmpty String lastName,
+                      @NotEmpty @Email String email) {
 
 }
