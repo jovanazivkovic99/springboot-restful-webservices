a9a2bf312ef261afc3074aaad68e5e66a43677f6
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-09-14 15:49:41 +0200

    Added swagger annotations

--- src/main/java/com/jovana/springbootrestfulwebservices/controller/UserController.java
+++ src/main/java/com/jovana/springbootrestfulwebservices/controller/UserController.java
@@ -2,6 +2,9 @@ package com.jovana.springbootrestfulwebservices.controller;
 
 import com.jovana.springbootrestfulwebservices.dto.UserDto;
 import com.jovana.springbootrestfulwebservices.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,32 +15,77 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(
+        name = "User Controller",
+        description = "Operations related to user"
+)
 public class UserController {
     
     private final UserService userService;
     
+    @Operation(
+            summary = "Create user",
+            description = "Save user to database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto createUser (@Valid @RequestBody UserDto user) {
         return userService.createUser(user);
     }
     
+    @Operation(
+            summary = "Get user by id",
+            description = "Get single user by id from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("/{id}")
     public UserDto getUserById (@PathVariable Long id) {
         return userService.getUserById(id);
     }
     
+    @Operation(
+            summary = "Get all users by id",
+            description = "Get all users by id from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping
     public List<UserDto> getAllUsers () {
         return userService.getAllUsers();
     }
     
+    
+    @Operation(
+            summary = "Update user by id",
+            description = "Update user in database by id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("/{id}")
     public UserDto updateUser (@PathVariable Long id, @Valid @RequestBody UserDto userDto) {
         
         return userService.updateUser(id, userDto);
     }
     
+    @Operation(
+            summary = "Delete user by id",
+            description = "Delete user in database by id"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @DeleteMapping("/{id}")
     public void deleteUser (@PathVariable Long id) {
         userService.deleteUser(id);
--- src/main/java/com/jovana/springbootrestfulwebservices/dto/UserDto.java
+++ src/main/java/com/jovana/springbootrestfulwebservices/dto/UserDto.java
@@ -1,13 +1,24 @@
 package com.jovana.springbootrestfulwebservices.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
+@Schema(
+        description = "UserDto info"
+)
 public record UserDto(Long id,
+                      @Schema(
+                              description = "User First Name"
+                      )
                       @NotEmpty(message = "User first name must not be null or empty.")
                       String firstName,
+                      
+                      @Schema(
+                              description = "User Last Name"
+                      )
                       @NotEmpty(message = "User last name must not be null or empty.")
                       String lastName,
                       @NotEmpty(message = "User email must not be null or empty.")
